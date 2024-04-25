43506937d93b06738c63cd3c3d00b8b24661f886
Mansi-bhandari1501 <mansi.bhandari1501@gmail.com>
2024-04-25 18:21:04 +0530

    first commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function NotFound(){
+    return(
+        <html>
+            <body className="text-center">
+                <h1 className="mt-20 font-bold">Something went wrong!</h1>
+            </body>
+        </html>
+    )
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+import { useTranslations } from "next-intl";
+import Link from "next/link";
+import LocalSwitcher from "./local-switcher";
+
+export default function Header(){
+    const t = useTranslations('Navigation');
+    return(
+        <header>
+            <nav className="flex items-center justify-between">
+                <Link href="/">{t('home')}</Link>
+                <LocalSwitcher/>
+            </nav>
+        </header>
+    )
+}
